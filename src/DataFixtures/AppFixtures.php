261fc80100a8585490919397edb4b487ade8c2ea
unknown <nuzzo.marcel@aliceadsl.fr>
2020-11-28 10:27:25 +0100

    make:user + fixtures for one user (admin)

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setFirstname("Marcel")
+            ->setLastname("Nuzzo")
+            ->setEmail("nuzzo.marcel@aliceadsl.fr")
+            ->setBirthdayAt(new \DateTime('1968-04-13'))
+            ->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                '1234'
+            ));
+         
+        
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
